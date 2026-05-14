1f7166b7c1a8b11f21253b2aca077912abc09920
Ashley Fong <akfkgx@mst.edu>
2026-05-14 13:02:31 -0500

    new page ?

---
+++ pages/meanings.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="hirono meanings",
+    page_icon="🧠",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+)
+
+st.markdown('<div class="main-title">Meanings Behind Hirono Figures</div>', unsafe_allow_html=True)
+
